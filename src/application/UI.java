7a2ba2ed76ad4e45a81b5aa635863d9c3008f980
RCamargo15 <rafael.camargo384@gmail.com>
2021-10-26 11:22:19 -0300

    Check and checkmate updates and a few other ones

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,8 +1,12 @@
 package application;
 
+import java.util.Arrays;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
@@ -47,6 +51,26 @@ public class UI {
 	}
 
 	// IMPRIMIR TABULEIRO
+	public static void printMatch(ChessMatch chessMatch, List<ChessPiece> captured) {
+		printBoard(chessMatch.getPieces());
+		System.out.println();
+		printCapturedPieces(captured);
+		System.out.println();
+		System.out.println("Turn: " + chessMatch.getTurn());
+		
+		if (!chessMatch.getCheckMate()) {
+		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+		}
+		
+		if (chessMatch.getCheck()) {
+			System.out.println("CHECK!");
+		}
+		else {
+			System.out.println("CHECKMATE!");
+			System.out.println("Winner: " + chessMatch.getCurrentPlayer());
+		}
+	}
+	
 	public static void printBoard(ChessPiece[][] pieces) {
 		for (int i = 0; i < pieces.length; i++) {
 
@@ -73,6 +97,21 @@ public class UI {
 		}
 		System.out.println("  A B C D E F G H");
 
+	}
+	
+	private static void printCapturedPieces (List <ChessPiece> captured) {
+		List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE).collect(Collectors.toList());
+		List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK).collect(Collectors.toList());
+		System.out.println("Captured pieces");
+		System.out.print("White: ");
+		System.out.print(ANSI_WHITE);
+		System.out.println(Arrays.toString(white.toArray()));
+		System.out.print(ANSI_RESET);
+		System.out.print("Black: ");
+		System.out.print(ANSI_GREEN);
+		System.out.println(Arrays.toString(black.toArray()));
+		System.out.print(ANSI_RESET);
+		
 	}
 
 	// IMPRIMIR PEÇA DE XADREZ
